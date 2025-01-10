538b9cbc9afac1f598c07a62aaf7fd993482b746
ScriptAutomate <derek@icanteven.io>
2025-01-10 17:42:44 -0600

    Prep Obsidian note system

--- init.lua
+++ init.lua
@@ -964,7 +964,23 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
-
+  -- { -- Note taking with obsidian
+  --   'epwalsh/obsidian.nvim',
+  --   version = 'v3.9.0',
+  --   lazy = true,
+  --   ft = 'markdown',
+  --   opts = {
+  --     workspaces = {
+  --       {
+  --         name = 'dendron',
+  --         path = '~/git/dendrons/nvim-dendron', -- Attempting to use Dendron vault
+  --         overrides = {
+  --           notes_subdir = 'notes',
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
